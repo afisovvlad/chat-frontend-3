bd9a1ab849c92c9a226f95953734747321460ee3
afisovvlad <afisovvlad@gmail.com>
2025-12-02 17:19:58 +0300

    intermedia

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,19 +1,56 @@
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import { ReactNode } from 'react';
 import cls from './Button.module.scss';
 
-interface ButtonProps {
-	isActive?: boolean;
-	disabled?: boolean;
+export enum ButtonColor {
+	PRIMARY = 'primary',
+	DANGER = 'danger',
+	GREEN = 'green'
 }
 
-export const Button = ({ isActive, disabled }: ButtonProps) => {
+export enum ButtonTheme {
+	CLEAR = 'clear',
+	OUTLINE = 'outline',
+	DISABLED = 'disabled',
+	BACKGROUND = 'background',
+	BACKGROUND_DISABLED = 'backgroundDisabled'
+}
+
+export enum ButtonSize {
+	S = 'size_s',
+	M = 'size_m',
+	L = 'size_l'
+}
+
+interface ButtonProps {
+	className?: string;
+	color?: ButtonColor;
+	theme?: ButtonTheme;
+	size?: ButtonSize;
+	disabled?: boolean;
+	children?: ReactNode;
+}
+
+export const Button = (props: ButtonProps) => {
+	const {
+		className,
+		color = ButtonColor.PRIMARY,
+		theme = ButtonTheme.BACKGROUND,
+		size = ButtonSize.L,
+		disabled,
+		children
+	} = props;
+
+	const mods: Mods = {
+		[cls[theme]]: true,
+		[cls[color]]: true,
+		[cls[size]]: true,
+		[cls.disabled]: disabled
+	};
+
 	return (
-		<div
-			className={classNames(
-				cls.Button,
-				{ isActive: isActive, disabled: disabled },
-				[]
-			)}
-		></div>
+		<button className={classNames(cls.Button, mods, [className])}>
+			{children}
+		</button>
 	);
 };
