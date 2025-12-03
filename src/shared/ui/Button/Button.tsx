1fbf6d98fa0838f42cd61372862b8832e4dd3ac5
afisovvlad <afisovvlad@gmail.com>
2025-12-03 19:59:55 +0300

    add button component, README file, add one rules for stylelint

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,21 +5,29 @@ import cls from './Button.module.scss';
 export enum ButtonColor {
 	PRIMARY = 'primary',
 	DANGER = 'danger',
-	GREEN = 'green'
+	GREEN = 'green',
+	TRANSPARENT = 'transparent'
 }
 
 export enum ButtonTheme {
 	CLEAR = 'clear',
 	OUTLINE = 'outline',
-	DISABLED = 'disabled',
 	BACKGROUND = 'background',
-	BACKGROUND_DISABLED = 'backgroundDisabled'
+	// преимущественно для кнопок внутри которых есть иконка
+	CIRCLE = 'circle'
 }
 
 export enum ButtonSize {
+	// в основном для кнопок в модалках
 	S = 'size_s',
 	M = 'size_m',
+	// в основном для кнопок на всю ширину родителя
 	L = 'size_l'
+}
+
+export enum ButtonFontSize {
+	S = 'fs_16',
+	M = 'fs_18'
 }
 
 interface ButtonProps {
@@ -27,7 +35,13 @@ interface ButtonProps {
 	color?: ButtonColor;
 	theme?: ButtonTheme;
 	size?: ButtonSize;
+	fontSize?: ButtonFontSize;
 	disabled?: boolean;
+	callBtn?: boolean;
+	widthDesktop?: string;
+	heightDesktop?: string;
+	widthMobile?: string;
+	heightMobile?: string;
 	children?: ReactNode;
 }
 
@@ -37,7 +51,13 @@ export const Button = (props: ButtonProps) => {
 		color = ButtonColor.PRIMARY,
 		theme = ButtonTheme.BACKGROUND,
 		size = ButtonSize.L,
-		disabled,
+		fontSize = ButtonFontSize.S,
+		disabled = false,
+		callBtn = false,
+		widthDesktop,
+		heightDesktop,
+		widthMobile,
+		heightMobile,
 		children
 	} = props;
 
@@ -45,11 +65,25 @@ export const Button = (props: ButtonProps) => {
 		[cls[theme]]: true,
 		[cls[color]]: true,
 		[cls[size]]: true,
+		[cls[fontSize]]: true,
+		[cls.callBtn]: callBtn,
 		[cls.disabled]: disabled
 	};
 
+	const buttonStyles = {
+		width: widthDesktop || undefined,
+		height: heightDesktop || undefined,
+		'@media (maxWidth: 768px)': {
+			width: widthMobile || undefined,
+			height: heightMobile || undefined
+		}
+	};
+
 	return (
-		<button className={classNames(cls.Button, mods, [className])}>
+		<button
+			className={classNames(cls.Button, mods, [className])}
+			style={buttonStyles}
+		>
 			{children}
 		</button>
 	);
