6418ff272d877032dca2a070b9198c442efcb9b7
Nina <frontend-developer@inbox.ru>
2025-12-06 11:04:06 +0300

    Добавила метаданные

--- app/(auth)/login/phone/page.tsx
+++ app/(auth)/login/phone/page.tsx
@@ -1,4 +1,11 @@
 import { LoginPhone } from '@/pages/LoginPhone/ui/LoginPhone';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Вход/Регистрация- Ввод телефона',
+	description: 'Ввод телефона на странице Вход/Регистрация',
+	keywords: 'А-Чат, вход, регистрация, ввести телефон'
+};
 
 export default function LoginPhonePage() {
 	return <LoginPhone />;
