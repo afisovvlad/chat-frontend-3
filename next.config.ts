22e0331a2326e3045c2f9ad4698886ccabab3085
afisovvlad <afisovvlad@gmail.com>
2025-12-06 13:14:10 +0300

    fix structure

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	compiler: {
-	styledComponents: true
-	reactStrictMode: true,
+		styledComponents: true
+	},
 
 	turbopack: {
 		rules: {
