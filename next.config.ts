6eeeaebb5045f9f179610fc66cedfe21c2e974c7
youngFrontendDeveloper <56949783+youngFrontendDeveloper@users.noreply.github.com>
2025-12-08 15:00:45 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,53 +1,41 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
 
-	compiler: {
-		styledComponents: true
-	},
-
-	// Турбопак включён по умолчанию — просто добавляем пустой объект,
-	// чтобы убрать ошибки о "webpack config detected".
-	turbopack: {}
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: { removeViewBox: false }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ],
+        as: '*.js'
+      }
+    }
+  }
 };
 
 export default nextConfig;
-
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-// 	compiler: {
-// 		styledComponents: true
-// 	},
-// 	reactStrictMode: true,
-
-// 	turbopack: {
-// 	rules: {
-// 		'*.svg': {
-// 			loaders: [
-// 				{
-// 					loader: '@svgr/webpack',
-// 					options: {
-// 						icon: true,
-// 						svgo: true,
-// 						svgoConfig: {
-// 							plugins: [
-// 								{
-// 									name: 'preset-default',
-// 									params: {
-// 										overrides: { removeViewBox: false }
-// 									}
-// 								}
-// 							]
-// 						}
-// 					}
-// 				}
-// 			],
-// 			as: '*.js'
-// 		}
-// 	}
-// }
-// };
-
-// export default nextConfig;
+в ts.config.ts  надо расширить алиас
+    "paths": {
+      "@/*": ["./src/*"],
+      "@icons/*": ["./public/assets/icons/*"]
+    }
