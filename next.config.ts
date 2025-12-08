56660a96b5b7e697948a1522fe4a3f06cd89e570
Nina <frontend-developer@inbox.ru>
2025-12-08 14:22:06 +0300

    Пробовала другие варианты next.config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,58 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-
 	compiler: {
 		styledComponents: true
 	},
-
-	// Турбопак включён по умолчанию — просто добавляем пустой объект,
-	// чтобы убрать ошибки о "webpack config detected".
-	turbopack: {}
+	reactStrictMode: true,
+	turbopack: {
+		rules: {
+			// ✅ Синтаксис правила исправлен
+			'*.svg': {
+				loaders: [
+					{
+						loader: '@svgr/webpack',
+						options: {
+							icon: true,
+							svgo: true,
+							svgoConfig: {
+								plugins: [
+									{
+										name: 'preset-default',
+										params: {
+											overrides: { removeViewBox: false }
+										}
+									}
+								]
+							}
+						}
+					}
+				],
+				as: '*.js'
+			}
+		}
+	}
 };
 
 export default nextConfig;
+
+// *************** ChatGPT ***************
+
+// const nextConfig: NextConfig = {
+// 	reactStrictMode: true,
+
+// 	compiler: {
+// 		styledComponents: true
+// 	},
+
+// 	// Турбопак включён по умолчанию — просто добавляем пустой объект,
+// 	// чтобы убрать ошибки о "webpack config detected".
+// 	turbopack: {}
+// };
+
+// export default nextConfig;
+
+// ****************** Айрат ***
 
 // import type { NextConfig } from 'next';
 
@@ -23,20 +63,20 @@ export default nextConfig;
 // 	reactStrictMode: true,
 
 // 	turbopack: {
-// 	rules: {
-// 		'*.svg': {
-// 			loaders: [
-// 				{
-// 					loader: '@svgr/webpack',
-// 					options: {
-// 						icon: true,
-// 						svgo: true,
-// 						svgoConfig: {
-// 							plugins: [
-// 								{
-// 									name: 'preset-default',
-// 									params: {
-// 										overrides: { removeViewBox: false }
+// rules: {
+// 	'*.svg': {
+// 		loaders: [
+// 			{
+// 				loader: '@svgr/webpack',
+// 				options: {
+// 					icon: true,
+// 					svgo: true,
+// 					svgoConfig: {
+// 						plugins: [
+// 							{
+// 								name: 'preset-default',
+// 								params: {
+// 									overrides: { removeViewBox: false }
 // 									}
 // 								}
 // 							]
