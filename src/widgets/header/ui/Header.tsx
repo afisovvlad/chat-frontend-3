6de7e1cb01a2c314824cfbaccb96cd4060ec85da
Nina <frontend-developer@inbox.ru>
2025-12-15 16:42:07 +0300

    Исправила замечания

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,15 +1,15 @@
-import styles from './Header.module.scss';
 import Apple from '@icons/Apple.svg';
-import Logo from '@icons/Logo.svg';
 import Google from '@icons/Google.svg';
+import Logo from '@icons/Logo.svg';
+import styles from './Header.module.scss';
 
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Logo height={44} width={44} fontSize={44} />
-			<div className={styles.riteBlock}>
-				<Apple height={44} width={150} />
-				<Google height={44} width={150} />
+			<Logo height={44} width={49} aria-label='Логотип' />
+			<div className={styles.rightBlock}>
+				<Apple height={44} width={150} aria-label='Скачать' />
+				<Google height={44} width={150} aria-label='Скачать' />
 			</div>
 		</header>
 	);
