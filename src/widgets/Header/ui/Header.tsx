c02eb09c3b52b9594806142d1799d5bf0fd8bb9b
Nina <frontend-developer@inbox.ru>
2025-12-15 18:06:23 +0300

    rename folder to Header with capital H

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,14 +1,10 @@
-import { Apple, Google, Logo } from '@icons/index';
 import styles from './Header.module.scss';
 
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Logo height={44} width={49} aria-label='Логотип' />
-			<div className={styles.rightBlock}>
-				<Apple height={44} width={150} aria-label='Скачать в AppStore' />
-				<Google height={44} width={150} aria-label='Найти в GooglePlay' />
-			</div>
+			{/* <Logo />
+			<DownloadLinks /> */}
 		</header>
 	);
 };
