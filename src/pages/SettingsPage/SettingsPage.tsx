a34ff4b675f247af263fadf58f5e4b3b7a52dac4
Airat <airat.24@mail.ru>
2026-01-09 14:44:47 +0300

    feat: add avatar upload with cropping functionality

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -13,32 +13,10 @@ import {
 	Text
 } from '@/shared/ui/Text';
 import { Trash } from '@icons/index';
-import {
-	AvatarUploader,
-	AvatarUploaderRef
-} from '@/shared/ui/ImageEditor/AvatarUpLoader/AvatarUpLoader';
-import { ButtonType } from '../../shared/ui/Button/model/types/type';
 
 import cls from './SettingsPage.module.scss';
 
 export const SettingsPage = () => {
-	const [avatar, setAvatar] = useState<string | null>(null);
-	const avatarRef = useRef<AvatarUploaderRef>(null);
-
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-
-		const formData = {
-			avatar: avatar // base64 строка или null
-		};
-
-		console.log('Данные для отправки Аватара:', formData);
-	};
-
-	const handleAvatarChange = (dataUrl: string) => {
-		setAvatar(dataUrl);
-	};
-
 	return (
 		<Container type={ContainerType.WRAPPER}>
 			<Container type={ContainerType.SIDEBAR} className={cls.sidebar}>
@@ -65,34 +43,6 @@ export const SettingsPage = () => {
 							<Text>+7 921 7797979</Text>
 							<Text>@bond777</Text>
 						</div>
-					</div>
-
-					<div>
-						<form onSubmit={handleSubmit}>
-							<div className={cls.btnWrapper}>
-								<Button
-									btnType={ButtonType.BUTTON}
-									onClick={() => avatarRef.current?.openFilePicker()}
-									className={cls.avatarBtn}
-								>
-									Изменить аватар
-								</Button>
-
-								<AvatarUploader
-									ref={avatarRef}
-									onAvatarChange={handleAvatarChange}
-									initialAvatar={avatar}
-								/>
-								<Button
-									btnType={ButtonType.SUBMIT}
-									theme={ButtonTheme.BACKGROUND}
-									color={ButtonColor.GREEN}
-									className={cls.avatarBtn}
-								>
-									Сохранить профиль
-								</Button>
-							</div>
-						</form>
 					</div>
 
 					<SettingsList />
