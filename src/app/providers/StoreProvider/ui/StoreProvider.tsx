fddd808bf3aee1321d0aef89771654c37064e49a
Nina <frontend-developer@inbox.ru>
2026-02-07 14:45:44 +0300

    redux-persist

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,21 +2,50 @@
 
 import { ReactNode, useRef } from 'react';
 import { Provider } from 'react-redux';
-import { StateSchema } from '../config/StateSchema';
-import { AppStore, makeStore } from '../config/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { makeStore } from '../config/store';
 
 interface StoreProviderProps {
 	children: ReactNode;
-	initialState?: DeepPartial<StateSchema>;
 }
 
-export function StoreProvider(props: StoreProviderProps) {
-	const { children, initialState } = props;
+export function StoreProvider({ children }: StoreProviderProps) {
+	const storeRef = useRef<ReturnType<typeof makeStore> | null>(null);
 
-	const storeRef = useRef<AppStore>(undefined);
 	if (!storeRef.current) {
-		storeRef.current = makeStore(initialState as StateSchema);
+		storeRef.current = makeStore();
 	}
 
-	return <Provider store={storeRef.current}>{children}</Provider>;
+	const { store, persistor } = storeRef.current;
+
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	);
 }
+
+// 'use client';
+
+// import { ReactNode, useRef } from 'react';
+// import { Provider } from 'react-redux';
+// import { StateSchema } from '../config/StateSchema';
+// import { AppStore, makeStore } from '../config/store';
+
+// interface StoreProviderProps {
+// 	children: ReactNode;
+// 	initialState?: DeepPartial<StateSchema>;
+// }
+
+// export function StoreProvider(props: StoreProviderProps) {
+// 	const { children, initialState } = props;
+
+// 	const storeRef = useRef<AppStore>(undefined);
+// 	if (!storeRef.current) {
+// 		storeRef.current = makeStore(initialState as StateSchema);
+// 	}
+
+// 	return <Provider store={storeRef.current}>{children}</Provider>;
+// }
