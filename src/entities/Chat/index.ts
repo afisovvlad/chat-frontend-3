7a2dfafc62601d784b49e408f3ddb43081412a0b
Airat <airat.24@mail.ru>
2026-02-12 16:27:04 +0300

    (feat search and chatList) fixed the search component and added a component chatlist

--- src/entities/Chat/index.ts
+++ src/entities/Chat/index.ts
@@ -7,35 +7,9 @@ export type {
 	Chat,
 	ChatUser,
 	ChatListResponse,
-	GetChatsRequest,
-	ChatState
+	GetChatsRequest
 } from './model';
 
 export { ChatType } from './model';
-
-export {
-	chatSlice,
-	chatReducer,
-	setSelectedChatId,
-	setChatLoading,
-	setChatError,
-	clearChatError,
-	addChat,
-	updateChat,
-	removeChat,
-	clearChats,
-	setChats
-} from './model';
-
-export {
-	selectChatState,
-	selectAllChats,
-	selectSelectedChatId,
-	selectSelectedChat,
-	selectFavoriteChats,
-	selectUnreadChats,
-	selectIsLoading,
-	selectError
-} from './model';
 
 export { mapChatToUserCard } from './lib/mapper/chatMapper';
