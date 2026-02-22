0c1c5b5b412427c7cacd3d920c8e9db4e2263aa0
afisovvlad <afisovvlad@gmail.com>
2026-02-22 15:15:45 +0300

    fix styles for button, add deleteProfile action

--- src/entities/Profile/api/editProfile.api.ts
+++ src/entities/Profile/api/editProfile.api.ts
@@ -30,10 +30,16 @@ export const profileApi = rtkApi.injectEndpoints({
 					dispatch(profileActions.setProfile(data));
 				} catch (_) {}
 			}
+		}),
+		deleteProfile: build.mutation({
+			query: () => ({
+				url: '/auth/messenger/profile/',
+				method: 'DELETE'
+			})
 		})
 	}),
 	// Отправка запроса за свежими данными
 	overrideExisting: true
 });
 
-export const { useEditProfileMutation } = profileApi;
+export const { useEditProfileMutation, useDeleteProfileMutation } = profileApi;
