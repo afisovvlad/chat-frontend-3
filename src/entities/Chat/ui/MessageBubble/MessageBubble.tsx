c18df024a395bffaf5dacd9198d36643d76b8737
asuna <cherepanova.so04@gmail.com>
2026-02-28 05:14:23 +0500

    chore(messageBubble): поддержка группового чата для

--- src/entities/Chat/ui/MessageBubble/MessageBubble.tsx
+++ src/entities/Chat/ui/MessageBubble/MessageBubble.tsx
@@ -1,5 +1,5 @@
 import { formatUnixToLocalTime } from '@/shared/lib/formatUnixToLocalTime/formatUnixToLocalTime';
-import { Text, TextColor, TextSize } from '@/shared/ui/Text';
+import { FontWeight, Text, TextColor, TextSize } from '@/shared/ui/Text';
 import { MessageStatusNode } from './MessageStatusNode';
 import './MessageBubble.scss';
 
@@ -9,6 +9,13 @@ interface MessageBubbleProps {
 	text: string;
 	status: 'received' | 'sending' | 'unread' | 'read';
 	onClick: (id: string) => void;
+
+	isGroupChat?: boolean;
+	senderName?: string;
+	senderAvatar?: string;
+
+	isFirstInGroup?: boolean;
+	isLastInGroup?: boolean;
 }
 
 export const MessageBubble = ({
@@ -16,42 +23,96 @@ export const MessageBubble = ({
 	time,
 	text,
 	status,
-	onClick
+	onClick,
+
+	isGroupChat = false,
+	senderName,
+	senderAvatar,
+	isFirstInGroup = false,
+	isLastInGroup = false
 }: MessageBubbleProps) => {
-	const messageClass = `message ${status !== 'received' ? 'message--sent' : 'message--received'}`;
+	const messageClass = `message ${
+		status !== 'received' ? 'message--sent' : 'message--received'
+	}`;
+
+	const isGroupReceived = isGroupChat && status === 'received';
+	const isSingle = isGroupReceived && isFirstInGroup && isLastInGroup;
+
+	const showName =
+		isGroupReceived && senderName && (isFirstInGroup || isSingle);
+
+	const showAvatar =
+		isGroupReceived && senderAvatar && (isLastInGroup || isSingle);
 
 	const handleActivate = () => {
 		onClick(id);
 	};
 
 	return (
-		<div
-			className={messageClass}
-			role='button'
-			tabIndex={0}
-			onClick={handleActivate}
-			onKeyDown={e => {
-				if (e.key === 'Enter' || e.key === ' ') {
-					e.preventDefault();
-					handleActivate();
-				}
-			}}
-		>
-			<Text lineHeight={1.3} color={TextColor.BLACK} className='message__text'>
-				{text}
-			</Text>
+		<div className='message-wrapper'>
+			<div className='message-row'>
+				{isGroupReceived && (
+					<div
+						className={`message-avatar-slot ${
+							showAvatar ? '' : 'message-avatar-slot--hidden'
+						}`}
+					>
+						{showAvatar && (
+							<img
+								src={senderAvatar}
+								alt={senderName}
+								className='message__avatar'
+							/>
+						)}
+					</div>
+				)}
 
-			<div className='message__meta'>
-				<Text
-					lineHeight={1.2}
-					fontSize={TextSize.S}
-					color={TextColor.GRAY}
-					className='message__time'
+				<div
+					className={messageClass}
+					role='button'
+					tabIndex={0}
+					onClick={handleActivate}
+					onKeyDown={e => {
+						if (e.key === 'Enter' || e.key === ' ') {
+							e.preventDefault();
+							handleActivate();
+						}
+					}}
 				>
-					{formatUnixToLocalTime(time)}
-				</Text>
+					{showName && (
+						<Text
+							fontWeight={FontWeight.BOLD}
+							fontSize={TextSize.S}
+							color={TextColor.ACCENT}
+							className='message__sender'
+						>
+							{senderName}
+						</Text>
+					)}
 
-				{status !== 'received' && <MessageStatusNode status={status} />}
+					<div className='message__content'>
+						<Text
+							lineHeight={1.3}
+							color={TextColor.BLACK}
+							className='message__text'
+						>
+							{text}
+						</Text>
+
+						<div className='message__meta'>
+							<Text
+								lineHeight={1.2}
+								fontSize={TextSize.S}
+								color={TextColor.GRAY}
+								className='message__time'
+							>
+								{formatUnixToLocalTime(time)}
+							</Text>
+
+							{status !== 'received' && <MessageStatusNode status={status} />}
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/entities/Messages/ui/Mesages/Messages.tsx
+++ src/entities/Messages/ui/Mesages/Messages.tsx
@@ -1,5 +1,51 @@
+import { MessageBubble } from '@/entities/Chat/ui/MessageBubble/MessageBubble';
 import styles from './Messages.module.scss';
 
 export function Messages() {
-	return <div className={styles.messages}>Messages</div>;
+	return (
+		<div className={styles.messages}>
+			<MessageBubble
+				id={'aaa'}
+				time={1772236560}
+				text={'hi'}
+				status={'received'}
+				onClick={() => {}}
+				isGroupChat={true}
+				senderName={'Stacy'}
+				senderAvatar={
+					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
+				}
+				isFirstInGroup={true}
+				isLastInGroup={false}
+			/>
+			<MessageBubble
+				id={'aaa'}
+				time={1772236560}
+				text={'hello'}
+				status={'received'}
+				onClick={() => {}}
+				isGroupChat={true}
+				senderName={'Stacy'}
+				senderAvatar={
+					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
+				}
+				isFirstInGroup={false}
+				isLastInGroup={false}
+			/>
+			<MessageBubble
+				id={'aaa'}
+				time={1772236560}
+				text={'hii'}
+				status={'received'}
+				onClick={() => {}}
+				isGroupChat={true}
+				senderName={'Stacy'}
+				senderAvatar={
+					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
+				}
+				isFirstInGroup={false}
+				isLastInGroup={true}
+			/>
+		</div>
+	);
 }
