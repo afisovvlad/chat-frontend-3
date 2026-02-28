3a035ae8ece5bc26c4c31b4201c7c90dee53c816
asuna <cherepanova.so04@gmail.com>
2026-02-28 16:43:27 +0500

    chore

--- src/entities/Messages/ui/Mesages/Messages.tsx
+++ src/entities/Messages/ui/Mesages/Messages.tsx
@@ -1,53 +1,7 @@
 'use client';
 
-import { MessageBubble } from '@/entities/Chat/ui/MessageBubble/MessageBubble';
 import styles from './Messages.module.scss';
 
 export function Messages() {
-	return (
-		<div className={styles.messages}>
-			<MessageBubble
-				id={'aaa'}
-				time={1772236560}
-				text={'hi'}
-				status={'received'}
-				onClick={() => {}}
-				isGroupChat={true}
-				senderName={'Stacy'}
-				senderAvatar={
-					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
-				}
-				isFirstInGroup={true}
-				isLastInGroup={false}
-			/>
-			<MessageBubble
-				id={'aaa'}
-				time={1772236560}
-				text={'hello'}
-				status={'received'}
-				onClick={() => {}}
-				isGroupChat={true}
-				senderName={'Stacy'}
-				senderAvatar={
-					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
-				}
-				isFirstInGroup={false}
-				isLastInGroup={false}
-			/>
-			<MessageBubble
-				id={'aaa'}
-				time={1772236560}
-				text={'hii'}
-				status={'received'}
-				onClick={() => {}}
-				isGroupChat={true}
-				senderName={'Stacy'}
-				senderAvatar={
-					'https://i.pinimg.com/736x/03/07/fd/0307fdc3552ddf80295158aea0737ae4.jpg'
-				}
-				isFirstInGroup={false}
-				isLastInGroup={true}
-			/>
-		</div>
-	);
+	return <div className={styles.messages}></div>;
 }
