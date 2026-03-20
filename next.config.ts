cded91687503a7e115acf4e490f4af3ff7061d32
afisovvlad <afisovvlad@gmail.com>
2026-03-20 19:54:36 +0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	experimental: {
+		optimizePackageImports: ['lucide-react', '@public/assets/icons']
+	},
 	images: {
 		domains: ['api.test.chat.ktsf.ru']
 	},
