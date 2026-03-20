ca38cd445a21fc0705f73ffed2132021ca7287eb
afisovvlad <afisovvlad@gmail.com>
2026-03-20 18:24:08 +0300

    update next version, add svgr/webpack

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	compiler: {
-		styledComponents: true
+	images: {
+		domains: ['api.test.chat.ktsf.ru']
 	},
-
 	turbopack: {
 		rules: {
 			'*.svg': {
@@ -12,53 +11,13 @@ const nextConfig: NextConfig = {
 					{
 						loader: '@svgr/webpack',
 						options: {
-							icon: true,
-							svgo: true,
-							svgoConfig: {
-								plugins: [
-									{
-										name: 'preset-default',
-										params: {
-											overrides: { removeViewBox: false }
-										}
-									}
-								]
-							}
+							icon: true
 						}
 					}
 				],
 				as: '*.js'
 			}
 		}
-	},
-	// тестовое
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'api.test.chat.ktsf.ru',
-				port: '',
-				pathname: '/media/**'
-			},
-			{
-				protocol: 'https',
-				hostname: 'interesnyefakty.org',
-				port: '',
-				pathname: '/wp-content/uploads/**'
-			}
-			// ← можно добавить другие хосты при необходимости
-		]
-	},
-	async headers() {
-		return [
-			{
-				source: '/api/proxy/:path*',
-				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
-					{ key: 'Access-Control-Allow-Origin', value: '*' } // В продакшене укажи конкретный домен
-				]
-			}
-		];
 	}
 };
 
