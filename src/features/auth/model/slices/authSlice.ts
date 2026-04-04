0b61e3b396f7ff0cca66212d2f5d6aff1773a538
afisovvlad <afisovvlad@gmail.com>
2026-04-04 16:40:43 +0300

    change authorization

--- src/features/auth/model/slices/authSlice.ts
+++ src/features/auth/model/slices/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AuthSchema } from '../types/AuthSchema';
+import { AuthSchema, PhoneSessionData } from '../types/AuthSchema';
 import { AuthStep } from '../types/authStep';
 
 const initialState: AuthSchema = {
@@ -51,8 +51,13 @@ const authSlice = createSlice({
 			state.phone_number = action.payload.phone_number;
 		},
 
+		setPhoneSession: (state, action: PayloadAction<PhoneSessionData>) => {
+			state.phoneSession = action.payload;
+		},
+
 		clearPhoneData: state => {
 			delete state.phone_number;
+			delete state.phoneSession;
 		},
 
 		setStep: (state, action: PayloadAction<AuthStep>) => {
